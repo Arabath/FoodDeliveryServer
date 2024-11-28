03705eb1a601e2da478dd8bf79cf68188b9e686a
Arabath <axelhobrecht@hotmail.com>
2024-11-28 13:49:39 -0300

    update

--- apps/users/src/users.module.ts
+++ apps/users/src/users.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 import { UsersService } from './users.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { UsersController } from './users.controller';
+import { ApolloFederationDriver, ApolloFederationDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: {
+        federation: 2,
+      },
+    }),
+  ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, ConfigService, JwtService],
 })
 export class UsersModule {}
